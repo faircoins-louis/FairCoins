a8adce911ccbb86dcff2768a4e3bacb1c7c6be0d
lg_99 <louisgautier99@gmail.com>
2016-08-30 22:57:29 +0200

    Test de commit

---
+++ FairCoins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FairCoins
+//
+//  Created by Jean-Noël Gautier on 29/08/2016.
+//  Copyright © 2016 Jean-Noël Gautier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
